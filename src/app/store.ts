681ac133b0d9cc0d9ee64d388cf07fa5e175cc58
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2022-11-09 10:34:19 +0100

    frontend added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,68 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+// import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+
+
+import { configureStore,  ThunkAction, Action ,combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import cartSlice from '../features/cart/cart-slice';
+import { productListSlice } from '../features/products/slice-list';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import orderListSlice from '../features/orders/slice-list';
+import productDetailsSlice from '../features/products/slice-details';
+import loginSlice from '../features/users/login-slice';
+import { authorizationProvider } from '../utils/auth-axios';
+import userDetailsSlice from '../features/users/user-details';
+import userListSlice from '../features/users/user-list';
+import userOrderSlice from '../features/orders/user-orders';
+import orderDetailSlice from '../features/orders/order-details';
+import productFilterSlice from '../features/products/search-list';
 import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+
+const reducers = combineReducers({
+  productList: productListSlice.reducer,
+  cart: cartSlice.reducer,
+  productDetail: productDetailsSlice.reducer,
+  productFilter: productFilterSlice.reducer,
+  //auth
+  login: loginSlice.reducer,
+  userDetails: userDetailsSlice.reducer,
+  userList: userListSlice.reducer,
+  //orders
+  orders: orderListSlice.reducer,
+  userOrder: userOrderSlice.reducer,
+  orderDetail: orderDetailSlice.reducer,
+  // counter 
+  counter: counterReducer,
 });
 
-export type AppDispatch = typeof store.dispatch;
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+authorizationProvider(store);
+
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from './app/store';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import { PersistGate } from 'redux-persist/integration/react'
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
